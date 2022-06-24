b5f794b7c4d1b41bc0aad7212d16306d04003eb9
JosephAshiyan <josephasiyan@gmail.com>
2022-06-24 14:35:29 +0300

    Meta Dragon React App generated

---
+++ README.old.md
@@ -0,0 +1,7 @@
+# Meta Dragon (Ejder Ötesi)
+
+The project of a meta workspace in a dragon world.
+
+First step of project is making a website
+
+After that we will generate the meta born drogon
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { FaDragon } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>
+          <FaDragon />
+          <hr />
+          Welcome To Meta Dragon
+          <hr />
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
