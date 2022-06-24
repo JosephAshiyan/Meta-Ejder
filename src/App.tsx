1d486a1f89b44189fb388e3a1a1efed871edfdfe
JosephAshiyan <josephasiyan@gmail.com>
2022-06-24 18:32:27 +0300

    App tsx changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
 import { FaDragon } from "react-icons/fa";
+import { SiDungeonsanddragons } from "react-icons/si";
+
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
         <h1>
-          <FaDragon />
+          <FaDragon font-size="120px"/>
           <hr />
           Welcome To Meta Dragon
           <hr />
+          The World of Truth
+          <hr />
+          <SiDungeonsanddragons font-size="120px"/>
         </h1>
       </div>
     </div>
